cf5fff4d49f4e369cb57e545abe6bd7643b5a232
Yscript <1187880666@qq.com>
2017-10-21 22:27:44 +0800

    figure.py: Creat to calculate the original data & analysis

---
+++ figure.py
@@ -0,0 +1,197 @@
+#! /usr/bin/env/python
+#encoding:utf-8
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+chainsFlie = "newChainInfo.txt"
+len_simuBox = 64
+height_simuBox = 64
+
+def builTheBox(len_simuBox,height_simuBox):
+	"""
+		the function make that building a simulation box 
+		and return the API : info_of_atoms which has record the information of all of atoms;
+	"""
+	number_totalAtoms = len_simuBox*len_simuBox*height_simuBox
+	info_of_atoms = []
+	iterNumber = 0
+	singleAtom = ["Sol",0,0,0,0]
+	for x in range(0,len_simuBox):
+		for y in range(0,len_simuBox):
+			for z in range(0,height_simuBox):
+				# singleAtom[0] = "Sol"  ## the comments code does not useful;
+				# singleAtom[1] = x 	## because of it reference delivery of list in python;
+				# singleAtom[2] = y
+				# singleAtom[3] = z
+				# singleAtom[4] = 0
+				singleAtom = ["Sol",x,y,z,0]
+				iterNumber = iterNumber + 1
+				info_of_atoms.append(singleAtom)			
+	if iterNumber != number_totalAtoms:
+		print("an error happens in the function of builTheBox")
+	print("the total atom number:",number_totalAtoms)
+	return info_of_atoms
+
+def dataImport(chainsFlie): 
+	"""
+		the function is made to read the datasteam from the original files,
+		which has recorded the chains' information about ,chainId and the site coordinates;
+		The API
+		parameters list and chainOrder list~list;
+	"""
+	newChainInfo = []
+	file = open(chainsFlie)
+	parameters = file.readline()
+	parameterList = parameters.split()
+	# confinedRadius = float(parameterList[0])
+	# confinedHeight = int(parameterList[1])
+	# polymerConcentration = float(parameterList[2])
+	# Asegment = int(parameterList[3])
+	# Bsegment = int(parameterList[4])
+	# lenOfPolymer = Asegment + Bsegment
+	# print(confinedRadius,confinedHeight,polymerConcentration,Asegment,Bsegment)
+	datas = file.readline()
+	dataList = datas.split()
+	numOfChain = int(dataList[-1])
+	parameterList.append(numOfChain)
+	print("the number of polymer:",numOfChain)
+	print(parameterList)
+	# the data read from the importfile is the type of "str" need to be translated as a "int" or "float" type.
+	iterNumber = 0
+	for lines in file.readlines():
+		chainDatas = lines.split()
+		newChainInfo.append(chainDatas)  #import the original datas into an list newChainInfo[];
+		iterNumber = iterNumber + 1
+		pass
+	if iterNumber != numOfChain:
+		print("error in read importFile" )
+		pass
+	file.close()
+	# print(len(chainDatas))
+	return parameterList,newChainInfo
+
+def meanSquareRee2andRg2(parameterList,newChainInfo,info_of_atoms):
+	"""
+		this function is used to handle and calculate the properities of 
+		the AB diblock copolymer chains,which Sol select A monomer;
+		just about the Rg2 and E-E2; 
+	"""
+	len_Asegment = int(parameterList[3])
+	len_Bsegment = int(parameterList[4])
+	lenOfPolymer = len_Asegment + len_Bsegment
+	# print(len_Asegment,len_Bsegment,lenOfPolymer)
+	# print(len(newChainInfo))
+	for x in range(0,len(newChainInfo)):
+		nct = 0
+		for y in newChainInfo[x]:
+			xi = info_of_atoms[int(y)][1]
+			yi = info_of_atoms[int(y)][2]
+			zi = info_of_atoms[int(y)][3]
+			ids = xi*len_simuBox*len_simuBox + yi*len_simuBox + zi
+			# conform the data correctly;
+			if int(y) != ids:
+				print("error in the transform the format of atoms")
+				pass
+			# judge the atom is A or B monomer;
+			if nct < (len_Asegment):
+				atom_name = "A"
+				atom_type= 190
+			else:
+				atom_name = "B"
+				atom_type = 110
+			nct = nct + 1
+			info_of_atoms[int(y)][0] = atom_name
+			info_of_atoms[int(y)][-1] = atom_type
+		pass
+		if nct != len(newChainInfo[x]):
+			print("error in for loop of function meanSquareRee2andRg2")
+	nct = nct *len(newChainInfo)	
+	print("number of atoms:",nct)
+	# check the data correctly;
+	num_A =0
+	num_B = 0
+	for x in range(len(info_of_atoms)):
+		if info_of_atoms[x][-1] == 190:	
+			num_A = num_A + 1
+		elif info_of_atoms[x][-1] == 110:
+			num_B = num_B + 1
+		pass
+	if (num_A+num_B) != nct:
+		print("error in function meanSquareRee2andRg2")
+	#  calculate the mean squre end-to-end distance
+	EE2 = []
+	Rg2 = []
+	height_simuBox = int(parameterList[1])
+	for chain in newChainInfo:
+		start_monomer = int(chain[len_Asegment])
+		end_monomer = int(chain[-1])
+		rx = info_of_atoms[start_monomer][1]-info_of_atoms[end_monomer][1]
+		ry = info_of_atoms[start_monomer][2]-info_of_atoms[end_monomer][2]
+		rz = info_of_atoms[start_monomer][3]-info_of_atoms[end_monomer][3]
+		rz = period_Boundary_condition(rz,height_simuBox)
+		rr2 = rx*rx + ry*ry + rz*rz
+		EE2.append(rr2)
+		pass
+	print(EE2)
+	meanSquareRee2 = float(sum(EE2)/len(EE2))
+	#  program for calculating the meanSquareRg2
+	for chain in newChainInfo:
+		vector_r2 =[]
+		for current_monomer in chain[len_Asegment:]:
+			for next_monomer in chain[len_Asegment:]:
+				rx = info_of_atoms[int(current_monomer)][1]-info_of_atoms[int(next_monomer)][1]
+				ry = info_of_atoms[int(current_monomer)][2]-info_of_atoms[int(next_monomer)][2]
+				rz = info_of_atoms[int(current_monomer)][3]-info_of_atoms[int(next_monomer)][3]
+				rz = period_Boundary_condition(rz,height_simuBox)
+				rr2 = rx*rx + ry*ry + rz*rz
+				vector_r2.append(rr2)
+				pass
+			pass
+		Rg2_of_singleChain = float(sum(vector_r2)/(2*len(chain)*len(chain)))
+		Rg2.append(Rg2_of_singleChain)
+		pass
+	meanSquareRg2 = float(sum(Rg2)/len(Rg2))
+	if len(Rg2) != len(EE2):
+		print("error in calculating Rg2&Ree2")
+	print("meanSquareRee2:",meanSquareRee2)
+	print("meanSquareRg2:",meanSquareRg2)
+
+def convert2CylidericalCoor():
+	pass
+
+
+def period_Boundary_condition(value,period):
+	half_period = int(period/2)
+	if value >half_period:
+		return value - period
+	elif value <= -half_period:
+		return value + period
+	else:
+		return value
+def dataExport():
+	rg2 = []
+	rg2_sum = 0
+	# open()
+	print("dataExport()")
+	pass
+
+def figures():
+	print("figures()")
+	pass
+
+def main():
+	info_of_atoms = builTheBox(len_simuBox,height_simuBox)
+	parameterList,newChainInfo = dataImport(chainsFlie)
+	chosenSegment_type = 2
+	meanSquareRee2andRg2(parameterList,newChainInfo,info_of_atoms)
+
+	dataExport()
+	figures()
+	print("main function()")
+	pass
+
+main()
+# if __name__ == '__main__':
+# 	main()
---
+++ rg2_mod.cpp
@@ -0,0 +1,316 @@
+#include <iostream>
+#include <fstream>
+/* the c++ produce aim to deal with the rg2 of grafted system;
+	respectively , ghp , BCP in the surface micelles , and in the Solutins;	
+	made by fengyuan ,2017/4/10.
+
+	using the files "chains.txt";
+	
+	the data of nchain[][] array is "id_atom -1";
+	the produce used in handling the rg2 is repeated, could be pro    moted;
+*/
+using namespace std;
+
+int const ndx = 3;
+int const len_fa = 11;
+int const len_fb = 9;
+int const len_ghp = 11;
+int const number_bcp = 480;
+int const zc = 10;
+int const hz = 60;
+double const BOND2 = 1.6258;
+
+struct coor
+{
+	int x;
+	int y;
+	int z;
+	int type;
+};
+int distance_r1_r2(int ,int ,int ,int ,int ,int ,int);
+
+int main(int argc, char const *argv[])
+{
+	ifstream fin;
+	ofstream fout,fo,fp;
+	
+	fin.open("chains.txt");
+    fo.open("chains_surf.txt");
+    fp.open("chains_sol.txt");
+    fout.open("rg2.txt");
+
+/*	fin.open("step_70.txt");
+    fo.open("step_70_surf.txt");
+    fp.open("step_70_sol.txt");
+    fout.open("step_70_rg2.txt");*/
+	int lx = 60, ly = 60 ,lz = hz;	//modify the hz value;
+	
+
+	int len_bcp = len_fa + len_fb;
+	int lxyz  = lx *ly *lz ;
+	int number_ghp = (lx*ly)/(ndx*ndx);
+	int number_total_chains = number_ghp + number_bcp;
+
+	int nct = 0;
+	coor *atom = new coor[lxyz];
+	for (int i = 0; i < lx; ++i)
+	{
+		for (int j = 0; j < ly; ++j)
+		{
+			for (int k = 0; k < lz; ++k)
+			{
+				atom[nct].x = i;
+				atom[nct].y = j;
+				atom[nct].z = k;
+				atom[nct].type = 0;
+				nct ++;
+			}
+		}
+	}
+
+	int **nchain;
+	nchain = new int*[number_total_chains];
+	for (int i = 0; i < number_total_chains; ++i)
+	{
+		nchain[i] = new int[len_bcp];
+	}
+	for (int i = 0; i < number_total_chains; ++i)
+	{
+		for (int j = 0; j < len_bcp; ++j)
+		{
+			nchain[i][j] = -1;			// easy to test;
+		}
+	}
+	/*the data stream come into three array */
+	int *ghp_surf = new int [number_ghp];
+	int *bcp_surf = new int [number_bcp];
+	int *bcp_sol = new int [number_bcp];
+
+	
+	// fin.open("step_30.txt");
+	/*string str;
+	fin >> str>>ndx;
+	fin >> str>>len_fa>>str>>len_fb;
+	fin >> str>>number_bcp;
+
+	cout << ndx <<"\t"<<len_fa<<"\t"<<len_fb<<"\t"<<number_bcp<<endl;*/
+	int len,id_chain,id_atom;
+	for (int i = 0; i < number_total_chains; ++i)
+	{
+		fin >>id_chain;
+		if ( i <number_ghp)
+		{
+			len = len_ghp;
+		}
+		else
+		{
+			len = len_bcp;
+		}
+
+		for (int j = 0; j < len; ++j)
+		{
+			fin >>id_atom;
+			nchain[i][j] = id_atom -1;	
+		}
+
+	}
+	fin.close();
+	for (int i = 0; i < number_ghp; ++i)
+	{
+		ghp_surf[i] = i; 		//record the id of 	ghp_surf;
+	}
+
+	bool criterion;	
+	int vartest,ncount;
+    ncount = 0 ;
+    nct = 0;
+    for (int i = 0; i < number_bcp; ++i)
+    {	
+        criterion = true;	
+    	id_chain = i + number_ghp;
+
+        for (int j = 0; j < len_fa; ++j)
+        {
+    	    id_atom = nchain[id_chain][j];
+            vartest = atom[id_atom].z;           
+            criterion = criterion&&(vartest<=zc && vartest >0);
+    	}   	
+        if(criterion){
+    		bcp_surf[ncount] = id_chain;    //record the id of chain bcp_surf;
+    		ncount ++;
+    	}
+        else{
+            bcp_sol[nct] = id_chain;		//record the id of chain bcp_sol;
+            nct ++;
+        }
+    }
+    int number_bcp_surf = ncount;
+    int number_bcp_sol = nct;
+    
+    nct =0;
+//    cout <<"number_ghp\t"<<number_ghp<<endl;
+//   cout <<"number_bcp_surf\t"<<number_bcp_surf<<endl;
+//   cout <<"number_bcp_sol\t"<<number_bcp_sol<<endl;
+
+    double rg2_ghp = 0.0;
+    double rg2_bcp = 0.0;
+    double rg2_bcp_surf = 0.0;
+    double rg2_bcp_sol = 0.0;
+    double rgi2;
+    int xk,yk,zk,xj,yj,zj;
+   
+/* ghp_surf*/
+    for (int i = 0; i < number_ghp; ++i)
+    {
+    	rgi2 = 0.0;
+    	for (int j = 0; j < len_ghp; ++j)
+    	{
+    		for (int k = 0; k < len_ghp; ++k)
+    		{
+    			xj =atom[nchain[i][j]].x;
+				yj =atom[nchain[i][j]].y;
+				zj =atom[nchain[i][j]].z;
+
+				xk =atom[nchain[i][k]].x;
+				yk =atom[nchain[i][k]].y;
+				zk =atom[nchain[i][k]].z;
+
+				rgi2 += distance_r1_r2(xj,yj,zj,xk,yk,zk,lx);
+    		}
+    	}
+    	rgi2 /= (double)(2*len_ghp*len_ghp);
+    	rg2_ghp +=rgi2; 
+    }
+    rg2_ghp =(double)(6*rg2_ghp)/(double)(number_ghp*(len_ghp-1)*BOND2);
+    cout <<"rg2_ghp\t"<<rg2_ghp<<endl;
+/*bcp_surf*/
+    for (int i = 0; i < number_bcp_surf; ++i)
+    {
+    	rgi2 = 0.0;
+    	id_chain = bcp_surf[i];
+    	for (int j = 0; j < len_fa; ++j)
+    	{
+    		for (int k = 0; k < len_fa; ++k)
+    		{
+    			xj =atom[nchain[id_chain][j]].x;
+				yj =atom[nchain[id_chain][j]].y;
+				zj =atom[nchain[id_chain][j]].z;
+
+				xk =atom[nchain[id_chain][k]].x;
+				yk =atom[nchain[id_chain][k]].y;
+				zk =atom[nchain[id_chain][k]].z;
+				rgi2 += distance_r1_r2(xj,yj,zj,xk,yk,zk,lx);
+    		}
+    	}
+    	rgi2 /= (double)(2*len_fa*len_fa);
+    	rg2_bcp_surf += rgi2;
+    }
+    rg2_bcp_surf =(double)(6*rg2_bcp_surf)/(double)(number_bcp_surf*(len_fa-0.5)*BOND2);
+    cout <<"rg2_bcp_surf\t"<<rg2_bcp_surf<<endl;
+/* bcp_sol*/
+    for (int i = 0; i < number_bcp_sol; ++i)
+    {
+    	rgi2 = 0.0;
+    	id_chain = bcp_sol[i];
+    	for (int j = 0; j < len_fa; ++j)
+    	{
+    		for (int k = 0; k < len_fa; ++k)
+    		{
+    			xj =atom[nchain[id_chain][j]].x;
+				yj =atom[nchain[id_chain][j]].y;
+				zj =atom[nchain[id_chain][j]].z;
+
+				xk =atom[nchain[id_chain][k]].x;
+				yk =atom[nchain[id_chain][k]].y;
+				zk =atom[nchain[id_chain][k]].z;
+				rgi2 += distance_r1_r2(xj,yj,zj,xk,yk,zk,lx);
+    		}
+    	}
+    	rgi2 /= (double)(2*len_fa*len_fa);
+    	rg2_bcp_sol += rgi2;
+    }
+    rg2_bcp_sol =(double)(6*rg2_bcp_sol)/(double)(number_bcp_sol*(len_fa-0.5)*BOND2);
+    
+    rg2_bcp = (rg2_bcp_surf*number_bcp_surf+rg2_bcp_sol*number_bcp_sol)/number_bcp;
+
+   cout <<"rg2_bcp_sol\t"<<rg2_bcp_sol<<endl;
+   cout <<"rg2_bcp\t"<<rg2_bcp<<endl;
+
+/*	output the data*/
+
+    fo <<number_bcp_surf<<endl;
+    for (int i = 0; i < number_bcp_surf; ++i)
+    {
+    	id_chain = bcp_surf[i];
+    	fo <<id_chain<<"\t";
+    	for (int j = 0; j < len_bcp; ++j)
+    	{
+    		fo <<nchain[id_chain][j]<<"\t";
+    	}
+    	fo <<endl;
+    }
+    fp <<number_bcp_sol<<endl;
+    for (int i = 0; i < number_bcp_sol; ++i)
+    {
+    	id_chain = bcp_sol[i];
+    	fp <<id_chain<<"\t";
+    	for (int j = 0; j < len_bcp; ++j)
+    	{
+    		fp <<nchain[id_chain][j]<<"\t";
+    	}
+    	fp <<endl;
+    }
+
+    fout <<"rg2_ghp\trg2_bcp_surf\trg2_bcp_sol\trg2_bcp"<<endl;
+    fout <<rg2_ghp<<"\t"<<rg2_bcp_surf<<"\t"<<rg2_bcp_sol<<"\t"<<rg2_bcp<<endl;
+    if (rg2_bcp_sol >=2.0 || rg2_bcp_surf >= 2.0 || rg2_bcp >=2.0)
+    {
+    	cout <<"error"<<endl;
+    }
+    fout.close();
+    fo.close();
+    fp.close();
+/*	free the storage*/
+	for (int i = 0; i < number_total_chains; ++i)
+	{
+		delete []nchain[i];
+	}
+	delete []nchain;
+
+	delete []atom;
+	atom = NULL;
+	delete []ghp_surf;
+	ghp_surf = NULL;
+	delete []bcp_surf;
+	bcp_surf = NULL;
+	delete []bcp_sol;
+	bcp_sol = NULL;
+	return 0;
+}
+
+int distance_r1_r2(int x1,int y1,int z1,int x2,int y2,int z2,int l){
+			int x = x1- x2;
+			int y = y1- y2;
+			int z = z1- z2;
+			int rr2;
+			if(x > l/2)
+			{	
+				x = x -l;
+			}
+			else if(x < -l/2)
+			{
+				x = x + l;
+			}
+			if(y >l/2)
+			{
+				y = y - l;
+			}
+			else if(y < -l/2)
+			{
+				y = y +l;		
+			}
+			rr2 = x*x + y*y+ z*z;
+	return rr2;
+}
+
+
